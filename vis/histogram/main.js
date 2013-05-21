7ee4f7e29b8a45012c10c7cce0e04b15642559e4
Zak King <zaking17@gmail.com>
2013-05-21 10:29:47 -0700

    Added histogram, scatter-trajectory, and box-and-whiskers plots.

---
+++ vis/histogram/main.js
@@ -0,0 +1,175 @@
+// data.json:
+// [
+//     {name:, x1:, x2: },
+//     ...
+// ]
+
+var default_filename = 0,
+x_axis_label = 'x',
+y_axis_label = 'frequency';
+
+// setup dropdown menu
+$.ajax({
+    dataType: "json",
+    url: 'http://zak.ucsd.edu/git/histogram/getdatafiles',
+    success:function(json){
+        $('#dropdown-menu').change( function() {
+            console.log('value: ' + $(this).val());
+            load_datafile($(this).val());
+        });
+        update_dropdown(json.data)
+        d3.json(json.data[default_filename], function(error, data) {
+            if (error) return console.warn(error);
+            setup_plot(data);
+        });
+    },
+    error:function(error) { console.log(error); }
+});
+
+// functions
+function load_datafile(this_file) {
+    d3.json(this_file, function(error, data) {
+        if (error) {
+            return console.warn(error);
+            $('body').append('error loading');
+        } else {
+            $('svg').remove();
+            setup_plot(data);
+        }
+    });
+}
+
+function update_dropdown(list) {
+    var menu = $('#dropdown-menu');
+    menu.empty();
+    for (var i=0; i<list.length; i++) {
+        menu.append("<option value="+list[i]+">"+list[i]+"</option>");
+    }
+    menu.focus();
+}
+
+function setup_plot(f) {
+    this_d = f
+
+    var margin = {top: 30, right: 20, bottom: 30, left: 40},
+    width = $(window).width() - 20 - margin.left - margin.right,
+    height = $(window).height() - 20 - margin.top - margin.bottom;
+
+    var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // var
+    x_dom = [d3.min([d3.min(f, function (d) { return d.x1; }), d3.min(f, function (d) { return d.x2; })]),
+                 d3.max([d3.max(f, function (d) { return d.x1; }), d3.max(f, function (d) { return d.x2; })]) ];
+
+    // var
+    x = d3.scale.linear()
+        .range([0, width])
+        .domain(x_dom);
+
+    x_size_scale = d3.scale.linear()
+        .range([0, width])
+	.domain([0, x_dom[1] - x_dom[0]]);
+
+    // Generate a histogram using twenty uniformly-spaced bins.
+
+    // var
+    data1 = d3.layout.histogram()
+        .value(function (d) { return d.x1; })
+        .bins(x.ticks(20))
+    (f);
+    var data2 = d3.layout.histogram()
+        .value(function (d) { return d.x2; })
+        .bins(x.ticks(20))
+    (f); 
+
+    // var
+    y = d3.scale.linear()
+        .domain([0, d3.max([data1, data2],
+                           function (a) {
+                               return d3.max(a, function(d) { return d.y; });
+                           })
+                ])
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+	.ticks(20);
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(20);
+
+    var diff = 10;
+    var bar_w = x_size_scale(data1[0].dx) - diff - 8;
+
+    var bar1 = svg.selectAll(".bar1")
+        .data(data1)
+        .enter().append("g")
+        .attr("class", "bar1")
+        .attr("transform", function(d) { return "translate(" + (x(d.x)+4) + "," + y(d.y) + ")"; });
+
+    bar1.append("rect")
+        .attr("x", 1)
+        .attr("width", bar_w)
+        .attr("height", function(d) { return height - y(d.y); });
+
+    var bar2 = svg.selectAll(".bar2")
+        .data(data2)
+        .enter().append("g")
+        .attr("class", "bar2")
+        .attr("transform", function(d) { return "translate(" + (x(d.x)+diff+4) + "," + y(d.y) + ")"; });
+
+    bar2.append("rect")
+        .attr("x", 1)
+        .attr("width", bar_w)
+        .attr("height", function(d) { return height - y(d.y); });
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("x", width)
+        .attr("y", -6)
+        .style("text-anchor", "end")
+        .text(x_axis_label);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text(y_axis_label)
+
+    var g = svg.append("g")
+	.attr("class", "legend")
+	.attr("transform", "translate(" + width/10 + ", 80)")
+	.attr("width", "300px")
+	.attr("height", "200px");
+
+    add_legend(g, '10x', 0, 'bar1');
+    add_legend(g, '100x', 1, 'bar2'); 
+    function add_legend(a, t, i, cl) {
+	var g = a.append("g")
+	    .attr("transform", "translate(0,"+i*40+")");
+	g.append("text")
+	    .text(t)
+	    .attr("transform", "translate(30, 14)");
+	g.append("rect")
+	    .attr("width", 15)
+	    .attr("height", 15)
+	    .attr("class", cl);
+    }
+
+}
