0d2d3d397efdf8c64a60ebdf08843d29210c4708
Zak King <zaking17@gmail.com>
2013-05-23 16:18:32 -0700

    Save tooltips with keystroke

--- vis/scatter/tooltip.js
+++ vis/scatter/tooltip.js
@@ -6,6 +6,9 @@
 
     node - Append the SVG objects to this node.
 */
+
+// TODO = move from global to 'tooltip' namespace
+
 function dist(x1, y1, x2, y2) {
     return Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
 }
@@ -25,16 +28,36 @@ var insert_linebreaks = function(t, text) {
     return words.length;
 };
 
-function cursor_tooltip(node, w, h, x_scale, y_scale) {
+var create_textspans = function(tooltip, loc) {
+    var texts = d3.selectAll('.point-circle')
+        .filter(function (d, i) {
+            var distance = dist(loc[0], loc[1], window.x_scale(d.f1), window.y_scale(d.f2));
+            return (distance < window.radius);
+        });
+    if (texts[0].length > 0) {
+        var this_text = "";
+        texts.each(function(d) {
+            this_text += d.name + '\n';
+        });
+        var l = insert_linebreaks(tooltip, this_text.trim());
+        tooltip.attr('dy', -l*text_height/2);
+    } else {
+        tooltip.text("");
+        tooltip.attr('dy', '0');
+    }
+}
+
+
+function cursor_tooltip(node, w, h, x_scale, y_scale, save_key) {
     var mouse_node = node.append('rect')
         .attr("width", w)
         .attr("height", h)
         .attr('style', 'visibility: hidden')
         .attr('pointer-events', 'all');
 
-    xg = x_scale; yg = y_scale
-
-    var r = 10;
+    window.x_scale = x_scale;
+    window.y_scale = y_scale;
+    window.radius = 10;
     var g = node
         .append('g')
         .attr('class', 'cursor-tooltip')
@@ -42,34 +65,53 @@ function cursor_tooltip(node, w, h, x_scale, y_scale) {
 
     var circle = g.append('circle')
         .attr('class','cursor-tooltip-circle')
-        .attr('r', r);
+        .attr('r', window.radius);
     var tooltip = g.append('g')
-        .attr('transform', 'translate('+(r+2)+',0)')
-	.append('text')
-	.attr('class', 'cursor-tooltip-text');
+        .attr('transform', 'translate('+(window.radius+2)+',0)')
+        .append('text')
+        .attr('class', 'cursor-tooltip-text');
     var play = false;
     window.setInterval(function(){play=true;}, 100);
     mouse_node.on('mousemove', function (d, i) {
-        var loc = d3.mouse(this);
+        window.loc = d3.mouse(this);
         if (play) {
             g.attr('transform', 'translate('+loc[0]+','+loc[1]+')');
-            var texts = d3.selectAll('.point-circle')
-                .filter(function (d, i) {
-                    var distance = dist(loc[0], loc[1], x_scale(d.f1), y_scale(d.f2));
-                    return (distance < r);
-                });
-            if (texts[0].length > 0) {
-                var this_text = "";
-                texts.each(function(d) {
-                    this_text += d.name + '\n';
-                });
-                var l = insert_linebreaks(tooltip, this_text.trim());
-                tooltip.attr('dy', -l*text_height/2);
-            } else {
-                tooltip.text("");
-                tooltip.attr('dy', '0');
-            }
+            create_textspans(tooltip, loc);
             play = false;
         }
     });
+
+    var saved_locs = [];
+    var saved_node = node.append('g').attr('id', 'saved_tooltips');
+    var update_circles = function(s) {
+        saved_node.selectAll('.saved_tooltip')
+            .data(s)
+            .enter()
+            .append('g')
+	    .attr('class', 'saved_tooltip')
+            .attr('transform', function (d) {
+                return 'translate('+d[0]+','+d[1]+')';
+            })
+            .call(add_tooltip);
+    }
+    if (save_key>=0) {
+        d3.select(window).on("keydown", function() {
+            if (d3.event.keyCode==save_key) {
+                saved_locs = saved_locs.concat([window.loc])
+		console.log(saved_locs);
+                update_circles(saved_locs);
+            }
+        });
+    }
+}
+
+function add_tooltip() {
+    this.append('circle')
+        .attr('class','cursor-tooltip-circle')
+        .attr('r', window.radius)
+    var tt = this.append('g')
+        .attr('transform', 'translate('+(window.radius+2)+',0)')
+        .append('text')
+        .attr('class', 'cursor-tooltip-text');
+    create_textspans(tt, this.datum());
 }
