ae2aedb38d25d8f00c965550b692fc6c04049591
Zak King <zaking17@gmail.com>
2013-06-24 15:05:28 -0700

    new namespacing and organization
    changes apply to subplot and scatter. need to propogate to others

--- vis/include/subplot.js
+++ vis/include/subplot.js
@@ -2,50 +2,67 @@
 // http://weblogs.asp.net/dwahlin/archive/2011/09/05/creating-multiple-javascript-objects-when-using-the-revealing-module-pattern.aspx
 
 var Subplot = function() {
-    var frames = [],            // subplot.get_frame({row:, column:}) or subplot.frames[row, col]
-    get_frame = function(o) {
-        return frames[o.row, o.column];
-    },
-    setup = function(options) {
-        // // some defaults
-        var rows = options.rows || 2,
-        columns = options.columns || 2,
-        selection = options.selection || d3.select("body"),
-        margin = 20;
+    var s = {};
 
-        if (options.fillScreen==true) {
+    s.frames = [];            // subplot.get_frame({row:, column:}) or subplot.frames[row, col]
+    s.get_frames = function() {
+	return s.frames;
+    };
+    s.get_frame = function(row, column) {
+        return s.frames[row][column];
+    };
+    s.update = function() {
+        if (s.fillScreen==true) {
             // d3.select('html').style('height','100%');
             // d3.select('body').style('height','100%');
-            selection.style('height', (window.innerHeight-margin)+'px');
-            selection.style('width', (window.innerWidth-margin)+'px');
+            s.selection.style('height', (window.innerHeight-s.margin)+'px');
+            s.selection.style('width', (window.innerWidth-s.margin)+'px');
         }
 
-        var height = parseFloat(selection.style('height')) - (margin),
-        width = parseFloat(selection.style('width')) - (margin),
-        row_h = height/rows,
-        col_w = width/columns;
+        var height = parseFloat(s.selection.style('height')) - (s.margin),
+            width = parseFloat(s.selection.style('width')) - (s.margin),
+            row_h = height/s.rows,
+            col_w = width/s.columns;
 
-        var data = [];
-        for (var y=margin; y<height; y+=row_h) {
+	d3.selectAll('.grid') 
+            .style('left',   function(d) { return Math.floor(d.x_i*col_w+s.margin)+'px'; })
+            .style('top',    function(d) { return Math.floor(d.y_i*row_h+s.margin)+'px'; })
+            .style('width',  function(d) { return Math.floor(col_w)+'px'; })
+            .style('height', function(d) { return Math.floor(row_h)+'px'; });
+        return this;
+    };
+    s.setup = function(options) {
+        // // some defaults
+        if (typeof options === 'undefined') options = {};
+        s.rows = options.rows || 2;
+        s.columns = options.columns || 2;
+        s.selection = options.selection || d3.select("body");
+        s.fillScreen = options.fillScreen || false;
+        s.margin = 20;
+
+        s.selection.empty();
+        s.frames = [];
+        for (var y=0; y<s.rows; y+=1) {
             // divide into rows
-            for (var x=margin; x<width; x+=col_w) {
+	    var a_row = [];
+            for (var x=0; x<s.columns; x+=1) {
                 // divide into columns
-                var fr = selection.append('div')
-                    .attr('class', 'grid')
-                    .style('position', 'absolute')
-                    .style('left', function(d) { return Math.floor(x)+'px' })
-                    .style('top', function(d) { return Math.floor(y)+'px' })
-                    .style('width', function(d) { return Math.floor(col_w)+'px' })
-                    .style('height', function(d) { return Math.floor(row_h)+'px' }); 
-		frames.push(fr);
+                var fr = s.selection.append('div')
+                        .attr('class', 'grid') 
+			.style('position', 'absolute')
+                        .datum({'x_i': x, 'y_i': y});
+                a_row.push(fr);
             }
-        } 
+	    s.frames.push(a_row)
+        }
+        s.update();
         return this;
     };
 
     return {
-        setup: setup,
-        frames: frames,
-        get_frame: get_frame
+        setup: s.setup,
+        get_frames: s.get_frames,
+        frame_by_row_col: s.get_frame,
+        update: s.update
     };
 };
