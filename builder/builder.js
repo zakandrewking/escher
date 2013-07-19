0d5f8e87dc82d0fb5d5a09dc38ba5252d1306e8f
Zachary King <zaking17@gmail.com>
2013-07-19 19:26:07 +0200

    line colors and flux labels

--- builder/builder.js
+++ builder/builder.js
@@ -5,6 +5,11 @@ var Builder = function() {
     m.newest_coords = [];
     m.reactions_drawn = [];
     m.cobra_model = [];
+    m.scale = {};
+    m.scale.flux_color = d3.scale.linear()
+        .domain([0, 1000])
+        .range(["blue", "red"]);
+    m.decimal_format = d3.format('.1f');
 
     m.load_list = function(coords) {
         // reloads data for autocomplete box and
@@ -36,9 +41,14 @@ var Builder = function() {
                 if (error) console.warn(error);
                 var json_f = [], num=20;
                 for (var i=0; i<json.length; i++) {
+                    m.cobra_model = m.cobra_model.map(function(x) {
+                        if (x.cobra_id == json[i][0]) x.flux = parseFloat(json[i][1]);
+                        return x;
+                    });
                     json_f = json_f.concat({ label: json[i][0]+" -- "+parseFloat(json[i][1]),
                                              value: json[i][0] });
                 }
+                console.log(m.cobra_model);
                 // set up the box with data, searching for first /num/ results
                 $("#rxn-input").autocomplete({ autoFocus: true,
                                                source: function(request, response) {
@@ -49,14 +59,14 @@ var Builder = function() {
                                                            for (var i=0; i<m.reactions_drawn.length; i++) {
                                                                if (m.reactions_drawn.cobra_id==x.value) return false;
                                                            }
-							   // match against entered string
+                                                           // match against entered string
                                                            return matcher.test(x.value);
                                                        });
                                                    response(results.slice(0,num));
                                                },
                                                change: function(event, ui) {
-						   if (ui.item) m.new_reaction(ui.item.value, coords);
-						   // d3.select(this).style('display', 'none');
+                                                   if (ui.item) m.new_reaction(ui.item.value, coords);
+                                                   // d3.select(this).style('display', 'none');
                                                    // $('#rxn-input').focus();
                                                }
                                              });
@@ -72,12 +82,11 @@ var Builder = function() {
 
     m.new_reaction = function(cobra_id, coords) {
         // new object at x, y /coords/
-	// if reaction id is new
+        // if reaction id is new
         if (m.reactions_drawn.filter(function(x) { return x.cobra_id==cobra_id; }).length==0) {
             m.reactions_drawn = m.reactions_drawn.concat({ cobra_id: cobra_id,
                                                            coords: m.align_to_grid(coords),
                                                            direction: 'down'});
-            console.log(m.reactions_drawn);
             var new_coords = m.update_circles();
             m.node_selected = cobra_id;
             m.load_list(new_coords);
@@ -86,7 +95,6 @@ var Builder = function() {
 
     m.update_circles = function(cobra_id) {
         var create_reaction = function(t) {
-            console.log(t);
             var d = t.datum(),
                 dis = 80,
                 r = 10,
@@ -107,46 +115,82 @@ var Builder = function() {
                 break;
             default: return;
             }
+
+            var reactions = m.cobra_model.filter(function (x) { return x.cobra_id==d.cobra_id; });
+            if (reactions.length!=1) {
+                console.warn('wrong # reactions: '+ reactions.length);
+                return;
+            }
+            var reaction = reactions[0],
+                reactants = reactions[0].metabolites.filter(function (x) {
+                    return x.coefficient < 0;
+                }),
+                products = reactions[0].metabolites.filter(function (x) {
+                    return x.coefficient > 0;
+                });
+            console.log('found ' + reactants.length + ' reactants');
+            console.log('found ' + products.length + ' products');
+
             var ds = r*3,
                 de = dis + r*3,
-                dc = dis + r*6;
+                dc = dis + r*6,
+                w = 80;
             li = [[Math.sin(angle) * ds, Math.cos(angle) * ds],
                   [Math.sin(angle) * de, Math.cos(angle) * de]];
             ci = [Math.sin(angle) * dc, Math.cos(angle) * dc];
-            this.append('path')
-                .attr('class', 'reaction-arrow')
-                .attr('d', d3.svg.line()(li))
-                .attr("marker-start", function (d) {
-                    return "url(#start)";
-                })
-                .attr("marker-end", function (d) {
-                    return "url(#end)";
-                });
-            this.append('circle')
-                .attr('r', r)
-                .attr('transform','translate(0, 0)');
 
-            function first_met_id(cobra_id, coefficient) {
-                try {
-                    var rxn = m.cobra_model.filter(function (x) { return x.cobra_id==d.cobra_id; })[0],
-                        met = rxn.metabolites.filter(function (x) { return x.coefficient==coefficient; })[0],
-                        me_id = met.cobra_id;
-                    return me_id;
-                } catch (e) { return null; }
+            var c;
+            if (reaction.flux) c = m.scale.flux_color(Math.abs(reaction.flux));
+            else c = 'black';
+            console.log(c);
+
+            function draw_reactant(g, reactant, index, count) {
+		var lia = [ [li[0][0] + (w*index - w*(count-1)/2), li[0][1]],
+			    [li[1][0] + (w*index - w*(count-1)/2), li[1][1]] ];
+                g.append('path')
+                    .attr('class', 'reaction-arrow')
+                    .attr('d', d3.svg.line()(lia))
+                    .attr("marker-start", function (d) {
+                        return "url(#start)";
+                    })
+                    .style('stroke', c);
+                var mg = g.append('g')
+                        .attr('transform','translate('+(w*index - w*(count-1)/2)+','+0+')');
+                mg.append('circle')
+                    .attr('r', r);
+                mg.append('text').text(reactant.cobra_id)
+                    .attr('class', 'metabolite-label')
+                    .attr('transform', 'translate(0, -18)');
             }
-            var met_id = first_met_id(cobra_id, -1);
-            if (met_id) {
-                this.append('text').text(met_id)
-                    .attr('transform', 'translate(10, 0)');
+            function draw_product(g, product, index, count) {
+		var lia = [ [li[0][0] + (w*index - w*(count-1)/2), li[0][1]],
+			    [li[1][0] + (w*index - w*(count-1)/2), li[1][1]] ];
+                g.append('path')
+                    .attr('class', 'reaction-arrow')
+                    .attr('d', d3.svg.line()(lia))
+                    .attr("marker-end", function (d) {
+                        return "url(#end)";
+                    })
+                    .style('stroke', c);
+                var mg = g.append('g')
+                        .attr('transform','translate('+(ci[0]+(w*index - w*(count-1)/2))+','+ci[1]+')');
+                mg.append('circle')
+                    .attr('r', r);
+                mg.append('text').text(product.cobra_id)
+                    .attr('class', 'metabolite-label')
+                    .attr('transform', 'translate(0, -18)');
             }
-            this.append('circle')
-                .attr('r', r)
-                .attr('transform','translate(' + ci[0] + ',' + ci[1] + ')');
-            met_id = first_met_id(cobra_id, 1);
-            if (met_id) {
-                this.append('text').text(met_id)
-                    .attr('transform', 'translate(' + (ci[0] + 10) + ',' + ci[1] + ')');
+            function draw_reaction_label(g, reaction) {
+                g.append('text')
+                    .attr('class', 'reaction-label')
+                    .text(reaction.cobra_id + " (" + m.decimal_format(reaction.flux) + ")")
+                    .attr('transform', 'translate('+(ci[0]+60)+','+(ci[1]/2)+')');
             }
+            var i = -1;
+            while (++i < reactants.length) draw_reactant(this, reactants[i], i, reactants.length);
+            i = -1;
+            while (++i < products.length) draw_product(this, products[i], i, products.length);
+            draw_reaction_label(this, reaction);
             m.newest_coords = [d.coords[0] + ci[0], d.coords[1] + ci[1]];
         };
         d3.select('#reactions')
