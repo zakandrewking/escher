ce5a5fdfea25f3c775a165145b7574d5ba698c86
Zak King <zaking17@gmail.com>
2013-10-04 16:20:00 -0700

    updated for utf-8 support

--- js/vis/export-svg.js
+++ js/vis/export-svg.js
@@ -1,14 +1,18 @@
 function ExportSvg () {
     var m = {};
+    m.utf8_to_b64 = function(str) {
+	return window.btoa(unescape(encodeURIComponent( str )));
+    };
     m.download = function(name, selection, do_beautify) {
-	console.log('selection');
         var a = document.createElement('a'), xml, ev;
         a.download = name+'.svg'; // file name
         xml = (new XMLSerializer()).serializeToString(d3.select(selection).node()); // convert node to xml string
-	console.log(xml);
 	if (do_beautify) xml = vkbeautify.xml(xml);
+	xml = '<?xml version="1.0" encoding="utf-8"?>\n \
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"\n \
+"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n' + xml;
         a.setAttribute("href-lang", "image/svg+xml");
-        a.href = 'data:image/svg+xml;base64,' + btoa(xml); // create data uri
+        a.href = 'data:image/svg+xml;base64,' + m.utf8_to_b64(xml); // create data uri
         // <a> constructed, simulate mouse click on it
         ev = document.createEvent("MouseEvents");
         ev.initMouseEvent("click", true, false, self, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
