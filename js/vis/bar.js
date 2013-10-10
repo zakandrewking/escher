0afa73b541174688123dd632d34133af4f7d9e65
Zak King <zaking17@gmail.com>
2013-10-10 14:44:39 -0700

    help from the new scaffold scripts

--- js/vis/bar.js
+++ js/vis/bar.js
@@ -1,296 +1,233 @@
-var Bar = function() {
-    var s = {};
-
-    s.height_width = function(fillScreen, sel, margins) {
-        if (fillScreen==true) {
-            sel.style('height', (window.innerHeight-margins.bottom)+'px');
-            sel.style('width', (window.innerWidth-margins.right)+'px');
-        }
-        var width = parseFloat(sel.style('width')) - margins.left - margins.right,
-            height = parseFloat(sel.style('height')) - margins.top - margins.bottom;
-        return {'width': width, 'height': height};
-    };
-
-    // s.update_size = function () {
-    //     // TODO inherit this function
-    //     var o = s.height_width(s.fillScreen, s.selection, s.margins);
-    //     var height = o.height, width = o.width;
-
-    //     var ns = s.selection.select("svg")
-    //             .attr("width", width + s.margins.left + s.margins.right)
-    //             .attr("height", height + s.margins.top + s.margins.bottom);
-    //     ns.select('g').attr("transform", "translate(" + s.margins.left + "," + s.margins.top + ")");
-
-    //     s.x.range([0, width]);
-    //     s.y.range([height, 0]);
-
-    //     s.xAxis.scale(s.x);
-    //     s.yAxis.scale(s.y);
-
-    //     s.selection.select('.x.hist-axis')
-    //         .attr("transform", "translate(0," + height + ")")
-    //         .call(s.xAxis)
-    //         .select('text')
-    //         .attr("x", width);
-    //     s.selection.select('.y.hist-axis')
-    //         .call(s.yAxis);
-
-    //     var bar_w = s.x(1) - s.diff - 8;
-
-    //     for (var i=0; i<s.json.length; i++) {
-    //         s.selection.selectAll(".bar.bar"+String(i))
-    //             .attr("transform", function(d) {
-    //                 return "translate(" + (s.x(d.x) + s.x_shift*i) + "," + s.y(d.y) + ")";
-    //             })
-    //             .select('rect')
-    //             .attr("width", bar_w)
-    //             .attr("height", function(d) { return height - s.y(d.y); });
-    //     }
-
-    //     d3.select(".legend") //TODO options for legend location
-    //         .attr("transform", "translate(" + (width-300) + ", 80)");
-
-    //     return this;
-    // };
-
-    s.update = function(json) {
-        // check data
-        var i=-1;
-        while(++i < json.length) {
-            if (json[i]===undefined) {
-                console.log('waiting for all indices');
-                return this;
-            }
-        }
-
-        // clear the container and add again
-        s.sub_selection.select("#bar-container").remove();
-        var container = s.sub_selection.append("g").attr("id","bar-container");
-        container.append("style")
-            .attr('type', "text/css")
-            .text(s.css);
-        var svg = container.append("g")
-                .attr("transform", "translate(" + s.margins.left + "," + s.margins.top + ")");
-
-        // var o = s.height_width(s.fillScreen, s.sub_selection, s.margins),
-        var height = s.height,
-            width = s.width;
-
-        // find x domain
-        var x_dom = [0, d3.max(json, function(a) { return a.data.length; })],
-            y_dom;
-        if (s.y_range) {
-            y_dom = s.y_range;
-        } else {
-            if (s.is_stacked) {
-                // sum up each data point
-                var i=-1, max = 0;
-                while (++i<json[0].data.length) {
-                    var j=-1, t = 0;
-                    while (++j<json.length) t += json[j].data[i].value;
-                    if (t > max) max = t;
-                }
-                y_dom = [
-                    d3.min(json, function(a) {
-                        return d3.min(a.data, function(d) { return d.value; });
-                    }),
-                    max
-                ];
-            } else {
-                y_dom = [
-                    d3.min(json, function(a) {
-                        return d3.min(a.data, function(d) { return d.value; });
-                    }),
-                    d3.max(json, function(a) {
-                        return d3.max(a.data, function(d) { return d.value; });
-                    })
-                ];
-            }
-        }
-
-        s.dom = {'y': y_dom,
-                 'x': x_dom};
-
-        s.x = d3.scale.linear()
-            .range([0, width])
-            .domain(x_dom).nice();
-        s.y = d3.scale.linear()
-            .range([height, 0])
-            .domain(s.dom.y).nice();
-
-        s.xAxis = d3.svg.axis()
-            .scale(s.x)
-            .orient("bottom")
-            .ticks(0);
-        s.yAxis = d3.svg.axis()
-            .scale(s.y)
-            .orient("left")
-            .tickFormat(d3.format("%"))
-            .ticks(5);
-
-        var legend = svg.append("g")
-                .attr("class", "legend")
-                .attr("transform", "translate(" + (width-300) + ", 80)")
-                .attr("width", "300px")
-                .attr("height", "200px");
-
-        var diff = 0,
-            bar_w = s.x(2) - s.x(1) - diff;
-
-        for (var j = 0; j < json.length; j++) {
-            var cl = 'bar'+String(j);
-            var bars = svg.selectAll("."+cl)
-                    .data(json[j].data)
-                    .enter().append("g")
-                    .attr("class", "bar "+cl);
-            if (s.is_stacked) {
-                if (j > 0) {
-                    bars.attr("transform", function(d, i) {
-                        return "translate(" + s.x(i) + "," + (s.y(d.value) - height + s.y(json[j-1].data[i].value)) + ")";
-                    });
-                } else {
-                    bars.attr("transform", function(d, i) { return "translate(" + s.x(i) + "," + s.y(d.value) + ")";  });
-                }
-            } else {
-                bars.attr("transform", function(d, i) { return "translate(" + (s.x(i) + s.x_shift*j) + "," + s.y(d.value) + ")";  });
-            }
-            var rects = bars.append("rect")
-                    .attr("x", 1)
-                    .attr("width", bar_w)
-                    .attr("height", function(d) { return height - s.y(d.value); })
-                    .style("fill", function(d) { if (s.color_scale) return s.color_scale(d.category);
-                                                 else return null; });
-        }
-
-        svg.append("g")
-            .attr("class", "x bar-axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(s.xAxis)
-            .append("text")
-            .attr("class", "label")
-            .attr("x", width)
-            .attr("y", -6)
-            .style("text-anchor", "end")
-            .text(s.x_axis_label);
-
-        svg.append("g")
-            .attr("class", "y bar-axis")
-            .call(s.yAxis)
-            .append("text")
-            .attr("class", "label")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", ".71em")
-            .style("text-anchor", "end")
-            .text(s.y_axis_label);
-
-        if (s.title) {
-            svg.append('text')
-		.attr('class', 'title')
-                .text(s.title)
-                .attr("transform", "translate("+width/2+",10)")
-                .attr("text-anchor", "middle");
-        }
-
-	if (s.update_hook) s.update_hook(svg);
-        return this;
-    };
-
-    s.setup = function (options) {
+define(["./scaffold", "lib/d3"], function (scaffold, d3) {
+    return function(options) {
         // set defaults
-        s.margins = {top: 10, right: 10, bottom: 10, left: 20};
-        s.fillScreen = false;
-        s.x_axis_label = "";
-        s.y_axis_label = "";
-        s.x_data_label = '1',
-        s.y_data_label = '2';
-        s.x_shift = 4;
-        s.data_is_object = true;
-        s.color_scale = false;
-        s.y_range = false;
-        s.title = false;
-        s.is_stacked = false;
-	s.update_hook = false;
-
-        // manage options
-        if (typeof options !== undefined) {
-            if (options.margins !== undefined)        s.margins = options.margins;
-            if (options.fillScreen !== undefined)     s.fillScreen = options.fillScreen;
-            if (options.x_axis_label !== undefined)   s.x_axis_label = options.x_axis_label;
-            if (options.y_axis_label !== undefined)   s.y_axis_label = options.y_axis_label;
-            if (options.y_data_label !== undefined)   s.y_data_label = options.y_data_label;
-            if (options.x_shift !== undefined)        s.x_shift = options.x_shift;
-            if (options.data_is_object !== undefined) s.data_is_object = options.data_is_object;
-            if (options.color_scale !== undefined)    s.color_scale = options.color_scale;
-            if (options.y_range !== undefined)        s.y_range = options.y_range;
-            if (options.title !== undefined)          s.title = options.title;
-            if (options.is_stacked !== undefined)     s.is_stacked = options.is_stacked;
-            if (options.update_hook !== undefined)    s.update_hook = options.update_hook;
-        }
-
-        // set selection
-        // sub selection places the graph in an existing svg environment
-        var add_svg = function(s, fillScreen, margins) {
-            var o = s.height_width(fillScreen, s, margins);
-            return s.append('svg')
-                .attr("width", o.width + margins.left + margins.right)
-                .attr("height", o.height + margins.top + margins.bottom)
-                .attr('xmlns', "http://www.w3.org/2000/svg");
-        };
-        if (options.sub_selection) {
-            s.sub_selection = options.sub_selection;
-            s.width = parseInt(s.sub_selection.attr("width"), 10) - s.margins.top - s.margins.bottom;
-            s.height = parseInt(s.sub_selection.attr("height"), 10) - s.margins.left - s.margins.right;
-        } else if (options.selection) {
-            s.sub_selection = add_svg(options.selection, s.fillScreen, s.margins);
-        } else {
-            s.sub_selection = add_svg(d3.select('body').append('div'), s.fillScreen,
-                                      s.margins);
-        }
-
+	var opt = scaffold.set_options(options, {
+            margins: {top: 10, right: 10, bottom: 10, left: 20},
+            fillScreen: false,
+            x_axis_label: "",
+            y_axis_label: "",
+            x_data_label: '1',
+            y_data_label: '2',
+            x_shift: 4,
+            data_is_object: true,
+            color_scale: false,
+            y_range: false,
+            title: false,
+            is_stacked: false,
+            update_hook: false,
+	    css: '' });
+ 
+	var out = scaffold.setup_svg(opt.selection, opt.sub_selection, 
+				     opt.margins, opt.fill_screen);
+	opt.svg = out.svg;
+	opt.height = out.height;
+	opt.width = out.width;
+	
         // load the css
-        if (options.css_path) {
-            s.ready = false;
-            d3.text(options.css_path, function(error, text) {
-                if (error) {
-                    console.warn(error);
-                    s.css = "";
-                } else {
-                    s.css = text;
-                }
-                s.ready = true;
-                return null;
-            });
-        } else {
-            s.ready = true;
-            s.css = "";
+	opt.ready = scaffold.load_css(opt.css_path, function(css) {
+	    opt.css = css;
+	    console.log('css ready'); 
+	    opt.ready = true; 
+	});
+	opt.layers = [];
+
+        var update_size = function () {
+	    var o = this.opt;
+
+	    out = scaffold.resize_svg(o.selection, o.sub_selection, o.margins, o.fill_screen);
+	    o.height = out.height;
+	    o.width = out.width;
+	    
+            // o.x.range([0, o.width]);
+            // o.y.range([o.height, 0]);
+
+            // o.xAxiscale(x);
+            // o.yAxiscale(y);
+
+            // o.svg.select('.x.hist-axis')
+            //     .attr("transform", "translate(0," + o.height + ")")
+            //     .call(o.xAxis)
+            //     .select('text')
+            //     .attr("x", o.width);
+            // o.svg.select('.y.hist-axis')
+            //     .call(o.yAxis);
+
+            // var bar_w = o.x(1) - o.diff - 8;
+
+            // for (var i=0; i<json.length; i++) {
+            //     selection.selectAll(".bar.bar"+String(i))
+            //         .attr("transform", function(d) {
+            //             return "translate(" + (x(d.x) + x_shift*i) + "," + y(d.y) + ")";
+            //         })
+            //         .select('rect')
+            //         .attr("width", bar_w)
+            //         .attr("height", function(d) { return height - y(d.y); });
+            // }
+
+            return this;
         };
-        s.json = [];
-        return this;
-    };
 
-    s.collect_data = function(json, layer) {
-        if (!s.ready) console.warn('Hasn\'t loaded css yet');
-        if (s.data_is_object) {
-            var objects = [];
-            for (var key in json) objects.push({name: key, value: json[key]});
-            s.json[layer] = {data: objects};
-        } else {
-            s.json[layer] = {data: json};
-        }
-        s.update(s.json);
-    };
+        var update = function() {
+	    var o = this.opt;
 
-    s.set_update_hook = function(fn) {
-	s.update_hook = fn;
-	return this;
-    };
+            // check data
+            var i=-1;
+            while(++i < o.layers.length) {
+                if (o.layers[i]===undefined) {
+                    console.log('waiting for all indices');
+                    return this;
+                }
+            }
 
-    return {
-        setup: s.setup,
-        update: s.update,
-        // update_size: s.update_size,
-        collect_data: s.collect_data,
-	update_hook: s.set_update_hook
+	    console.log(o.width, o.height);
+
+            // clear the container and add again
+            o.svg.select("#bar-container").remove();
+            var container = o.svg.append("g").attr("id","bar-container");
+            container.append("style")
+                .attr('type', "text/css")
+                .text(o.css);
+            var svg = container.append("g")
+                    .attr("transform", "translate(" + o.margins.left + "," + o.margins.top + ")");
+
+            // find x domain
+            var x_dom = [0, d3.max(o.layers, function(a) { return a.data.length; })],
+                y_dom;
+            if (o.y_range) {
+                y_dom = o.y_range;
+            } else {
+                if (o.is_stacked) {
+                    // sum up each data point
+                    var i=-1, max = 0;
+                    while (++i<o.layers[0].data.length) {
+                        var j=-1, t = 0;
+                        while (++j<o.layers.length) t += o.layers[j].data[i].value;
+                        if (t > max) max = t;
+                    }
+                    y_dom = [
+                        d3.min(o.layers, function(a) {
+                            return d3.min(a.data, function(d) { return d.value; });
+                        }),
+                        max
+                    ];
+                } else {
+                    y_dom = [
+                        d3.min(o.layers, function(a) {
+                            return d3.min(a.data, function(d) { return d.value; });
+                        }),
+                        d3.max(o.layers, function(a) {
+                            return d3.max(a.data, function(d) { return d.value; });
+                        })
+                    ];
+                }
+            }
+
+            var dom = {'y': y_dom,
+                     'x': x_dom},
+
+            x = d3.scale.linear()
+                .range([0, o.width])
+                .domain(x_dom).nice(),
+            y = d3.scale.linear()
+                .range([o.height, 0])
+                .domain(dom.y).nice(),
+            xAxis = d3.svg.axis()
+                .scale(x)
+                .orient("bottom")
+                .ticks(0),
+            yAxis = d3.svg.axis()
+                .scale(y)
+                .orient("left")
+                .tickFormat(d3.format("%"))
+                .ticks(5);
+
+            var diff = 0,
+                bar_w = x(2) - x(1) - diff;
+
+            for (var j = 0; j < o.layers.length; j++) {
+                var cl = 'bar'+String(j);
+                var bars = svg.selectAll("."+cl)
+                        .data(o.layers[j].data)
+                        .enter().append("g")
+                        .attr("class", "bar "+cl);
+                if (o.is_stacked) {
+                    if (j > 0) {
+                        bars.attr("transform", function(d, i) {
+                            return "translate(" + x(i) + "," + (y(d.value) - o.height + y(o.layers[j-1].data[i].value)) + ")";
+                        });
+                    } else {
+                        bars.attr("transform", function(d, i) { return "translate(" + x(i) + "," + y(d.value) + ")";  });
+                    }
+                } else {
+                    bars.attr("transform", function(d, i) { return "translate(" + (x(i) + o.x_shift*j) + "," + y(d.value) + ")";  });
+                }
+                var rects = bars.append("rect")
+                        .attr("x", 1)
+                        .attr("width", bar_w)
+                        .attr("height", function(d) { return o.height - y(d.value); })
+                        .style("fill", function(d) { if (o.color_scale) return o.color_scale(d.category);
+                                                     else return null; });
+            }
+
+            svg.append("g")
+                .attr("class", "x bar-axis")
+                .attr("transform", "translate(0," + o.height + ")")
+                .call(xAxis)
+                .append("text")
+                .attr("class", "label")
+                .attr("x", o.width)
+                .attr("y", -6)
+                .style("text-anchor", "end")
+                .text(o.x_axis_label);
+
+            svg.append("g")
+                .attr("class", "y bar-axis")
+                .call(yAxis)
+                .append("text")
+                .attr("class", "label")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", ".71em")
+                .style("text-anchor", "end")
+                .text(o.y_axis_label);
+
+            if (o.title) {
+                svg.append('text')
+                    .attr('class', 'title')
+                    .text(o.title)
+                    .attr("transform", "translate("+o.width/2+",10)")
+                    .attr("text-anchor", "middle");
+            }
+
+            if (o.update_hook) o.update_hook(svg);
+            return this;
+        };
+
+        var collect_data = function(json, layer) {
+	    var o = this.opt;
+            if (!o.ready) console.warn('Hasn\'t loaded css yet');
+            if (o.data_is_object) {
+                var objects = [];
+                for (var key in json) objects.push({name: key, value: json[key]});
+                o.layers[layer] = {data: objects};
+            } else {
+                o.layers[layer] = {data: json};
+            }
+            this.update();
+	    return this;
+        };
+
+        var set_update_hook = function(fn) {
+	    var o = this.opt;
+            o.update_hook = fn;
+            return this;
+        };
+
+        return {
+	    opt: opt,
+            update: update,
+            collect_data: collect_data,
+            update_hook: set_update_hook
+        };
     };
-};
+});
---
+++ js/vis/scaffold.js
@@ -0,0 +1,100 @@
+define(["lib/d3"], function(d3) {
+    // private
+    var height_width_style = function(selection, margins) {   
+        var width = parseFloat(selection.style('width')) - margins.left - margins.right,
+	    height = parseFloat(selection.style('height')) - margins.top - margins.bottom;
+	console.log(selection);
+        return {'width': width, 'height': height};
+    };
+    var height_width_attr = function(selection, margins) {   
+        var width = parseFloat(selection.attr('width')) - margins.left - margins.right,
+	    height = parseFloat(selection.attr('height')) - margins.top - margins.bottom;
+	console.log(selection);
+        return {'width': width, 'height': height};
+    };
+
+    // public
+    var set_options = function(options, defaults) {
+	var i = -1, 
+	    out = defaults,
+	    keys = window.Object.keys(options);
+	while (++i < keys.length) out[keys[i]] = options[keys[i]];
+	return out;
+    };
+    var setup_svg = function(selection, sub_selection, margins, fill_screen) {
+        // sub selection places the graph in an existing svg environment
+        var add_svg = function(f, s, m) {
+            if (f) {
+		s.style('height', (window.innerHeight-margins.bottom)+'px');
+		s.style('width', (window.innerWidth-margins.right)+'px');
+            }
+            var out = height_width_style(f, s, margins);
+	    out.svg = s.append('svg')
+                .attr("width", out.width + m.left + m.right)
+                .attr("height", out.height + m.top + m.bottom)
+                .attr('xmlns', "http://www.w3.org/2000/svg");
+	    return out;
+        };
+
+	// run
+	var out;
+        if (sub_selection) {
+            out = height_width_attr(sub_selection, margins);
+	    out.svg = sub_selection;
+        } else if (selection) {
+            out = add_svg(fill_screen, selection, margins);
+        } else {
+            out = add_svg(fill_screen, d3.select('body').append('div'), margins);
+        }
+	return out;
+    };
+
+    var resize_svg = function(selection, sub_selection, margins, fill_screen) {
+	// returns null
+      var resize = function(f, s, m) {
+            if (f) {
+		s.style('height', (window.innerHeight-margins.bottom)+'px');
+		s.style('width', (window.innerWidth-margins.right)+'px');
+            }
+            var out = height_width_style(f, s, margins);
+	    out.svg = s.select('svg')
+                .attr("width", out.width + m.left + m.right)
+                .attr("height", out.height + m.top + m.bottom)
+                .attr('xmlns', "http://www.w3.org/2000/svg");
+	    return out;
+        };
+
+	var out;
+	if (sub_selection) {
+            out = height_width_attr(sub_selection, margins);
+	    out.svg = sub_selection;
+        } else if (selection) {
+            out = resize(fill_screen, selection, margins);
+        } else {
+            out = resize(fill_screen, d3.select('body').append('div'), margins);
+        }
+	return out;
+    };
+
+    var load_css = function(css_path, callback) {
+	var css = "";
+        if (css_path) {
+            d3.text(css_path, function(error, text) {
+                if (error) {
+                    console.warn(error);
+                } 
+                css = text;
+                callback(css);
+            });
+        }
+        return null;
+    };
+    var update = function () {
+	return 'omg yes';
+    };
+    return {
+	set_options: set_options,
+	setup_svg: setup_svg,
+	load_css: load_css
+    };
+});
