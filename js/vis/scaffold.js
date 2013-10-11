2177434223510a026a1bedfeb297f5df33bc027f
Zachary King <zaking17@gmail.com>
2013-10-11 00:20:15 -0700

    adapting maps in progress

--- js/vis/scaffold.js
+++ js/vis/scaffold.js
@@ -2,85 +2,85 @@ define(["lib/d3"], function(d3) {
     // private
     var height_width_style = function(selection, margins) {
         var width = parseFloat(selection.style('width')) - margins.left - margins.right,
-	    height = parseFloat(selection.style('height')) - margins.top - margins.bottom;
+            height = parseFloat(selection.style('height')) - margins.top - margins.bottom;
         return {'width': width, 'height': height};
     };
-    var height_width_attr = function(selection, margins) {   
+    var height_width_attr = function(selection, margins) {
         var width = parseFloat(selection.attr('width')) - margins.left - margins.right,
-	    height = parseFloat(selection.attr('height')) - margins.top - margins.bottom;
+            height = parseFloat(selection.attr('height')) - margins.top - margins.bottom;
         return {'width': width, 'height': height};
     };
 
     // public
     var set_options = function(options, defaults) {
-	var i = -1, 
-	    out = defaults,
-	    keys = window.Object.keys(options);
-	while (++i < keys.length) out[keys[i]] = options[keys[i]];
-	return out;
+        var i = -1,
+            out = defaults,
+            keys = window.Object.keys(options);
+        while (++i < keys.length) out[keys[i]] = options[keys[i]];
+        return out;
     };
     var setup_svg = function(selection, selection_is_svg, margins, fill_screen) {
         // sub selection places the graph in an existing svg environment
         var add_svg = function(f, s, m) {
             if (f) {
-		s.style('height', (window.innerHeight-margins.bottom)+'px');
-		s.style('width', (window.innerWidth-margins.right)+'px');
+                s.style('height', (window.innerHeight-margins.bottom)+'px');
+                s.style('width', (window.innerWidth-margins.right)+'px');
             }
             var out = height_width_style(s, m);
-	    out.svg = s.append('svg')
+            out.svg = s.append('svg')
                 .attr("width", out.width + m.left + m.right)
                 .attr("height", out.height + m.top + m.bottom)
                 .attr('xmlns', "http://www.w3.org/2000/svg");
-	    return out;
+            return out;
         };
 
-	// run
-	var out;
+        // run
+        var out;
         if (selection_is_svg) {
             out = height_width_attr(selection, margins);
-	    out.svg = selection;
+            out.svg = selection;
         } else if (selection) {
             out = add_svg(fill_screen, selection, margins);
         } else {
             out = add_svg(fill_screen, d3.select('body').append('div'), margins);
         }
-	return out;
+        return out;
     };
 
     var resize_svg = function(selection, selection_is_svg, margins, fill_screen) {
-	// returns null
-      var resize = function(f, s, m) {
+        // returns null
+        var resize = function(f, s, m) {
             if (f) {
-		s.style('height', (window.innerHeight-margins.bottom)+'px');
-		s.style('width', (window.innerWidth-margins.right)+'px');
+                s.style('height', (window.innerHeight-margins.bottom)+'px');
+                s.style('width', (window.innerWidth-margins.right)+'px');
             }
             var out = height_width_style(f, s, margins);
-	    out.svg = s.select('svg')
+            out.svg = s.select('svg')
                 .attr("width", out.width + m.left + m.right)
                 .attr("height", out.height + m.top + m.bottom)
                 .attr('xmlns', "http://www.w3.org/2000/svg");
-	    return out;
+            return out;
         };
 
-	var out;
-	if (selection_is_svg) {
+        var out;
+        if (selection_is_svg) {
             out = height_width_attr(selection, margins);
-	    out.svg = selection;
+            out.svg = selection;
         } else if (selection) {
             out = resize(fill_screen, selection, margins);
         } else {
             out = resize(fill_screen, d3.select('body').append('div'), margins);
         }
-	return out;
+        return out;
     };
 
     var load_css = function(css_path, callback) {
-	var css = "";
+        var css = "";
         if (css_path) {
             d3.text(css_path, function(error, text) {
                 if (error) {
                     console.warn(error);
-                } 
+                }
                 css = text;
                 callback(css);
             });
@@ -88,11 +88,29 @@ define(["lib/d3"], function(d3) {
         return false;
     };
     var update = function () {
-	return 'omg yes';
+        return 'omg yes';
+    };
+    var load_the_file = function(file, callback) {
+	if (file) d3.json(file, callback);
+	else callback(null, "No filename");
+    };
+    var load_files = function(files_to_load, callback) {
+	// load multiple files asynchronously
+	// Takes a list of objects: { file: a_filename.json, callback: a_callback_fn }
+        var i = -1, remaining = files_to_load.length;
+        while (++i < files_to_load.length) {
+	    var this_file = files_to_load[i];
+            load_the_file(this_file.file,
+                          function(d, error) {
+                              this_file.callback(d, error);
+                              if (!--remaining) callback();
+                          });
+	}
     };
     return {
-	set_options: set_options,
-	setup_svg: setup_svg,
-	load_css: load_css
+        set_options: set_options,
+        setup_svg: setup_svg,
+        load_css: load_css,
+        load_files: load_files
     };
 });
