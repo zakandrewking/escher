65afd0e1e2207acc061a0959144a31403a197427
Zak King <zaking17@gmail.com>
2013-10-17 18:20:05 -0700

    removed old redundant python folder

--- python/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-
-setup(name='visbio',
-      version='0.2',
-      author='Zachary King',
-      url='http://zakandrewking.github.io/visbio/',
-      packages=['visbio'],
-      package_data={'visbio': ['static/*']})
--- python/visbio/__init__.py
+++
@@ -1,1 +0,0 @@
-from visbio import Map, maps_dir
