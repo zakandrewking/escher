92cd2a1bc8718f709ddff127c7a07889dfb3042b
Zak King <zaking17@gmail.com>
2014-03-28 11:55:40 -0700

    another bug with the build

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-define(["builder/Builder", "builder/Viewer", "builder/KeyManager", "vis/data-menu"],
+define(["builder/Builder", "builder/KeyManager", "vis/data-menu"],
        function(bu, vi, km, dm) {
            return { Builder: bu,
-		    Viewer: vi,
 		    KeyManager: km,
 		    DataMenu: dm };
        });
