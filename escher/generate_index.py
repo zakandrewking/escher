8802b5249325559543835f5fb35a4e4254acbcbb
Zachary King <zaking17@gmail.com>
2014-09-07 19:12:56 -0700

    fixed error

--- escher/generate_index.py
+++ escher/generate_index.py
@@ -2,14 +2,14 @@ import json
 from os import listdir
 from os.path import join, dirname, realpath
 
-directory = realpath(dirname(realpath(__file__)), '..')
+directory = realpath(join(dirname(realpath(__file__)), '..'))
 
 index_directories = ['maps/v1', 'models/v1']
 
 for index_dir in index_directories:
     index = []
     for filename in listdir(join(directory, index_dir)):
-        if filename.endswith('.json'):
+        if filename.endswith('.json') and filename not in ['index.json', 'README']:
             index.append(filename.replace('.json', ''))
     with open(join(directory, index_dir, 'index.json'), 'w') as f:
         json.dump(index, f)
