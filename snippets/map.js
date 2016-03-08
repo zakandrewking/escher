981d8deb0f70ac3dfaf969d1e44498aec869e4da
Zachary King <zaking17@gmail.com>
2016-03-08 09:46:18 -0800

    biojs snippet

---
+++ snippets/map.js
@@ -0,0 +1,11 @@
+/* global rootDiv, d3 */
+
+var escher = require('escher-vis')
+var url = 'https://escher.github.io/1-0-0/5/maps/Escherichia coli/e_coli_core.Core metabolism.json'
+
+d3.json(url, function (error, map_data) {
+  escher.Builder(map_data, null, null, d3.select(rootDiv), {
+    fill_screen: true,
+    menu: 'zoom',
+  })
+})
