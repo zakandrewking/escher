433b187f103b3af4de10cdd57cd3681d41313cbc
Zachary King <zaking17@gmail.com>
2016-10-25 23:24:40 -0400

    tooltip, now with content!

--- js/src/Tooltip.js
+++ js/src/Tooltip.js
@@ -1,8 +1,47 @@
+/* global d3, window, XMLHttpRequest */
+
+/**
+ * Define a Tooltip component and interface with Tinier.
+ */
+
 var createComponent = require('tinier').createComponent
 var createInterface = require('tinier').createInterface
 var interfaceTypes = require('tinier').interfaceTypes
 var h = require('tinier-dom').h
 var render = require('tinier-dom').render
+
+var containerStyle = {
+  'min-width': '300px',
+  'min-height': '120px',
+  'border-radius': '4px',
+  'border': '2px solid #333',
+  'padding': '7px',
+  'background-color': '#fff',
+  'opacity': '0.9',
+  'text-align': 'left',
+  'font-size': '18px',
+  'font-family': 'sans-serif',
+  'color': '#111',
+}
+
+var idStyle = {
+  'font-size': '22px',
+}
+
+var buttonStyle = {
+  'border-radius': '3px',
+  'background-color': '#eee',
+  'border': '1px solid #ddd',
+}
+
+function getButtonText (status, biggId) {
+  if (status === 'error') {
+    return biggId + ' not found in BiGG Models'
+  } else {
+    return ((status === 'checking' ? '(Checking) ' : '') +
+            'Open ' + biggId + ' in BiGG Models.')
+  }
+}
 
 var DefaultTooltip = createComponent({
   displayName: 'DefaultTooltip',
@@ -11,22 +50,60 @@ var DefaultTooltip = createComponent({
   init: function (arg) { return arg },
   signalNames: [ 'changeIsPrimary', 'didHide', ],
 
+  reducers: {
+    changeBiggStatus: function (args) {
+      return Object.assign({}, args.state, {
+        status: args.status,
+        url: args.status === 'found' ? args.url : null,
+      })
+    },
+  },
+
+  methods: {
+    checkOpenBigg: function (args) {
+      var url = ('http://bigg.ucsd.edu/universal/' + args.state.type + 's/' +
+                 args.state.biggId)
+      if (args.state.status === 'found') {
+        window.open(url)
+      } else {
+        args.reducers.changeBiggStatus({ status: 'checking' })
+        const req = new XMLHttpRequest()
+        d3.xhr(url, function (error, data) {
+          if (error) {
+            args.reducers.changeBiggStatus({ status: 'error' })
+          } else {
+            window.open(url)
+            args.reducers.changeBiggStatus({ status: 'found', url: url })
+          }
+        })
+      }
+    },
+  },
+
   render: function (args) {
     var isPrimary = args.state.isPrimary
     return render(
       args.el,
       h('div',
-        null,
-        args.state.bigg_id,
+        { style: containerStyle },
+        h('span', { style: idStyle }, args.state.biggId),
+        h('br'),
+        'name: ' + args.state.name,
+        h('br'),
+        'data: ' + (args.state.data ? args.state.data : 'no data'),
+        h('br'),
         h('button',
-          { onClick: function () { args.signals.changeIsPrimary() } }))
+          { style: buttonStyle,
+            onClick: (args.state.status === 'checking' ? null :
+                      args.methods.checkOpenBigg), },
+          getButtonText(args.state.status, args.state.biggId)))
     )
   },
 })
 
 // var TooltipInterface = createInterface({
 //   state: {
-//     bigg_id: interfaceTypes.string,
+//     biggId: interfaceTypes.string,
 //     name: interfaceTypes.string,
 //     loc: {
 //       x: interfaceTypes.number,
--- js/src/TooltipContainer.js
+++ js/src/TooltipContainer.js
@@ -1,11 +1,15 @@
-/** TooltipContainer */
-
-var utils = require('./utils');
-var PlacedDiv = require('./PlacedDiv');
+var utils = require('./utils')
+var PlacedDiv = require('./PlacedDiv')
 var tinier = require('tinier')
 
-
-var TooltipContainer = utils.make_class();
+/**
+ * Manage the tooltip that lives in a PlacedDiv.
+ * @param selection
+ * @param map
+ * @param tooltip_component
+ * @param zoom_container
+ */
+var TooltipContainer = utils.make_class()
 // instance methods
 TooltipContainer.prototype = {
   init: init,
@@ -14,16 +18,16 @@ TooltipContainer.prototype = {
   is_visible: is_visible,
   show: show,
   hide: hide,
-};
-module.exports = TooltipContainer;
-
+}
+module.exports = TooltipContainer
 
 // definitions
-function init(selection, map, tooltip_component, zoom_container) {
-  var div = selection.append('div')
-        .attr('id', 'tooltip-container')
+function init (selection, map, tooltip_component, zoom_container) {
+  var div = selection.append('div').attr('id', 'tooltip-container')
   this.placed_div = PlacedDiv(div, map)
   this.placed_div.hide()
+
+  div.on('mouseleave', this.hide.bind(this))
 
   this.map = map
   this.setup_map_callbacks(map)
@@ -33,9 +37,11 @@ function init(selection, map, tooltip_component, zoom_container) {
   // keep a reference to tinier tooltip
   var initialState = {
     isVisible: false,
-    bigg_id: null,
+    biggId: null,
     name: null,
     loc: null,
+    data: null,
+    status: null,
   }
   this.tinier_tooltip = tinier.run(tooltip_component, div.node(),
                                    { initialState: initialState })
@@ -44,36 +50,45 @@ function init(selection, map, tooltip_component, zoom_container) {
 function setup_map_callbacks(map) {
   map.callback_manager.set('show_tooltip.tooltip_container',
                            this.show.bind(this))
-  map.callback_manager.set('hide_tooltip.tooltip_container',
-                           this.hide.bind(this))
+  // map.callback_manager.set('hide_tooltip.tooltip_container',
+  //                          this.hide.bind(this))
 }
 
 function setup_zoom_callbacks(zoom_container) {
   zoom_container.callback_manager.set('zoom.tooltip_container', function() {
-  }.bind(this));
+    if (this.is_visible()) {
+      this.hide()
+    }
+  }.bind(this))
   zoom_container.callback_manager.set('go_to.tooltip_container', function() {
-  }.bind(this));
+    if (this.is_visible()) {
+      this.hide()
+    }
+  }.bind(this))
 }
 
 /**
  * Return visibility of tooltip container.
  * @return {Boolean} Whether tooltip is visible.
  */
-function is_visible() {
+function is_visible () {
   return this.placed_div.is_visible()
 }
 
 /**
  * Show and place the input.
  */
-function show(type, d) {
+function show (type, d) {
   if (type === 'reaction_label') {
-    var coords = { x: d.label_x, y: d.label_y }
+    var coords = { x: d.label_x, y: d.label_y + 10 }
     this.placed_div.place(coords)
     var new_state = {
-      bigg_id: d.bigg_id,
+      biggId: d.bigg_id,
       name: d.name,
       loc: coords,
+      data: d.data,
+      type: 'reaction',
+      status: null,
     }
     this.tinier_tooltip.setState(new_state)
   } else {
@@ -84,7 +99,7 @@ function show(type, d) {
 /**
  * Hide the input.
  */
-function hide() {
+function hide () {
   this.placed_div.hide()
   this.tinier_tooltip.signals.didHide.call()
 }
