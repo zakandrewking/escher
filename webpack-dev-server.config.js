546e03610f2ea564fabfbe75083ecde774847583
spaghettikinase <gouzusama@gmail.com>
2017-08-10 11:19:59 -0700

    Got dev server working with Babel

--- webpack-dev-server.config.js
+++ webpack-dev-server.config.js
@@ -1,16 +1,24 @@
 module.exports = {
-  entry: './js/lib/devServer.js',
+  entry: './js/src/devServer.js', // TODO try changing lib/ to src/ and adding babel here
   devtool: 'source-map',
   devServer: {
     contentBase: './dev-server',
     open: true,
     port: 7621
   },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: ['babel-loader']
       }
     ]
   },
