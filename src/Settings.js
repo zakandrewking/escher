c32d1aaa4111862430cadd647bef1bdec6e90048
Zak King <zaking17@gmail.com>
2017-10-25 17:10:59 -0700

    logging & style

--- src/Settings.js
+++ src/Settings.js
@@ -15,24 +15,20 @@
 
  */
 
-var utils = require('./utils');
+import utils from './utils'
+import bacon from 'baconjs'
 
-var bacon = require('baconjs');
-
-
-var Settings = utils.make_class();
+var Settings = utils.make_class()
 // instance methods
 Settings.prototype = {
-    init: init,
-    set_conditional: set_conditional,
-    hold_changes: hold_changes,
-    abandon_changes: abandon_changes,
-    accept_changes: accept_changes
-};
-module.exports = Settings;
+  init: init,
+  set_conditional: set_conditional,
+  hold_changes: hold_changes,
+  abandon_changes: abandon_changes,
+  accept_changes: accept_changes
+}
+module.exports = Settings
 
-
-// instance methods
 function init (set_option, get_option, conditional_options) {
   this.set_option = set_option
   this.get_option = get_option
@@ -64,111 +60,122 @@ function init (set_option, get_option, conditional_options) {
  * Hold on to event when hold_property is true, and only keep them if
  * accept_property is true (when hold_property becomes false).
  */
-function _convert_to_conditional_stream (status_stream) {
-
-    // true if hold is pressed
-    var is_not_hold_event = status_stream
-            .map(function(x) { return x=='hold'; })
-            .not()
-            .toProperty(true),
-        is_not_first_clear = status_stream
-            .scan(false, function(c, x) {
-                // first clear only
-                return (c==false && (x=='accepted' || x=='rejected'));
-            }).not(),
-        is_not_first_hold = status_stream
-            .scan(false, function(c, x) {
-                // first clear only
-                return (c==false && x=='hold');
-            }).not(),
-        combined = bacon.combineAsArray(this, status_stream),
-        held = combined
-            .scan([], function(c, x) {
-                if (x[1]=='hold') {
-                    c.push(x[0]);
-                    return c;
-                } else if (x[1]=='accept') {
-                    return c;
-                } else if (x[1]=='reject') {
-                    return [];
-                } else if (x[1]=='rejected' || x[1]=='accepted') {
-                    return [x[0]];
-                } else {
-                    throw Error('bad status ' + x[1]);
-                }
-            })
-    // don't pass in events until the end of a hold
-            .filter(is_not_hold_event)
-    // ignore the event when clear is passed
-            .filter(is_not_first_clear)
-    // ignore the event when hold is passed
-            .filter(is_not_first_hold)
-            .flatMap(function(ar) {
-                return bacon.fromArray(ar);
-            }),
-        unheld = this.filter(is_not_hold_event);
-    return unheld.merge(held);
+function _convert_to_conditional_stream (status_stream, name) {
+  // true if hold is pressed
+  const is_not_hold_event = status_stream
+          .map(x => x === 'hold')
+          .not()
+          .toProperty(true)
+  const is_not_first_clear = status_stream
+          .scan(false, function(c, x) {
+            // first clear only
+            return (c==false && (x=='accepted' || x=='rejected'))
+          }).not()
+  const is_not_first_hold = status_stream
+          .scan(false, function(c, x) {
+            // first clear only
+            return (c==false && x=='hold')
+          }).not()
+  const combined = bacon.combineAsArray(this, status_stream)
+  const held = combined
+          .scan([], function(c, x) {
+            if (name === 'allow_building_duplicate_reactions') console.log(c, x)
+            if (x[1] === 'hold') {
+              c.push(x[0])
+              return c
+            } else if (x[1] === 'accept') {
+              return c
+            } else if (x[1]=='reject') {
+              return []
+            } else if (x[1]=='rejected' || x[1]=='accepted') {
+              return [x[0]]
+            } else {
+              throw Error('bad status ' + x[1])
+            }
+          })
+  // don't pass in events until the end of a hold
+          .filter(is_not_hold_event)
+  // ignore the event when clear is passed
+          .filter(is_not_first_clear)
+  // ignore the event when hold is passed
+          .filter(is_not_first_hold)
+          .flatMap(function(ar) {
+            return bacon.fromArray(ar)
+          })
+  const unheld = this.filter(is_not_hold_event)
+  return unheld.merge(held)
 }
 
-function _force_update_with_bus(bus) {
-    return bacon
-        .combineAsArray(this, bus.toProperty(false))
-        .map(function(t) {
-            return t[0];
-        });
+/**
+ * Combine the stream with another stream (bus) that is used to force a new
+ * value.
+ */
+function _force_update_with_bus (bus) {
+  return bacon
+    .combineAsArray(this, bus.toProperty(false))
+    .map(t => t[0])
 }
 
-function _create_conditional_setting(name, initial_value, set_option,
-                                     status_bus, force_update_bus) {
-    // set up the bus
-    var bus = new bacon.Bus();
-    // make the event stream
-    var stream = bus
-    // conditionally accept changes
-            .convert_to_conditional_stream(status_bus)
-    // force updates
-            .force_update_with_bus(force_update_bus);
+function _create_conditional_setting (name, initial_value, set_option,
+                                      status_bus, force_update_bus) {
+  // set up the bus
+  var bus = new bacon.Bus()
+  // make the event stream
+  var stream = bus
+  // conditionally accept changes
+        .convert_to_conditional_stream(status_bus, name)
+  // force updates
+        .force_update_with_bus(force_update_bus)
 
-    // get the latest
-    stream.onValue(function(v) {
-        set_option(name, v);
-    });
+  // get the latest
+  bus.onValue(function (v) {
+    if (name === 'allow_building_duplicate_reactions') console.log('bus', name, v)
+    set_option(name, v)
+  })
 
-    // push the initial value
-    bus.push(initial_value);
+  // get the latest
+  stream.onValue(function (v) {
+    if (name === 'allow_building_duplicate_reactions') console.log('stream', name, v)
+    set_option(name, v)
+  })
 
-    return { bus: bus, stream: stream };
+  // push the initial value
+  bus.push(initial_value)
+
+  return { bus: bus, stream: stream }
 }
 
-function set_conditional(name, value) {
-    /** Set the value of a conditional setting, one that will only be
-     accepted if this.accept_changes() is called.
+/** Set the value of a conditional setting, one that will only be
+ accepted if this.accept_changes() is called.
 
-     Arguments
-     ---------
+ Arguments
+ ---------
 
-     name: The option name
+ name: The option name
 
-     value: The new value
+ value: The new value
 
-     */
-
-    if (!(name in this.busses))
-        throw new Error('Invalid setting name');
-    this.busses[name].push(value);
+ */
+function set_conditional (name, value) {
+  if (!(name in this.busses))
+    throw new Error('Invalid setting name')
+  this.busses[name].push(value)
 }
 
-function hold_changes() {
-    this.status_bus.push('hold');
+function hold_changes () {
+  console.log('hold')
+  this.status_bus.push('hold')
 }
 
-function abandon_changes() {
-    this.status_bus.push('reject');
-    this.status_bus.push('rejected');
-    this.force_update_bus.push(true);
+function abandon_changes () {
+  console.log('abandon')
+  this.status_bus.push('reject')
+  this.status_bus.push('rejected')
+  this.force_update_bus.push(true)
 }
 
 function accept_changes() {
-    this.status_bus.push('accept');
-    this.status_bus.push('accepted');
+  console.log('accept')
+  this.status_bus.push('accept')
+  this.status_bus.push('accepted')
 }
