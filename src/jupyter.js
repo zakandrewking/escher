035649e00c1dcd645ffe26a562592f485f1985ef
Zachary King <zaking17@gmail.com>
2018-06-26 19:12:55 -0700

    implement basic jupyter widget view

--- src/jupyter.js
+++ src/jupyter.js
@@ -1,13 +1,34 @@
+/* global ESCHER_VERSION */
+
+import { default as Builder } from './Builder'
+import { select as d3Select } from 'd3-selection'
+
 import { DOMWidgetView, DOMWidgetModel } from '@jupyter-widgets/base'
+
+const version = ESCHER_VERSION
 
 /**
  * @jupyter-widgets/base is optional, so only initialize if it's called
  */
 export default function initializeJupyterWidget () {
-  class EscherMapView extends DOMWidgetView {}
-  class EscherMapModel extends DOMWidgetModel {}
-  return {
-    EscherMapView,
-    EscherMapModel
+  class EscherMapModel extends DOMWidgetModel {
+    constructor () {
+      super()
+      this._model_name = 'EscherMapModel'
+      this._view_name = 'EscherMapView'
+      this._model_module = 'escher'
+      this._view_module = 'escher'
+      this._model_module_version = version
+      this._view_module_version = version
+      this.value = 'Hello World'
+    }
   }
+
+  class EscherMapView extends DOMWidgetView {
+    render () {
+      Builder(null, null, null, d3Select(this.el), {})
+    }
+  }
+
+  return { EscherMapView, EscherMapModel }
 }
